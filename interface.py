7a1e4d80ebc0e2168721790a3d77d4e70d0978c3
Pedro Vinícius <127950680+Pvini124@users.noreply.github.com>
2026-05-31 14:54:23 -0300

    Adição das funções restantes e uma melhora no layout

--- interface.py
+++ interface.py
@@ -10,76 +10,137 @@ criar_tabela()
 #-----------------------
 
 janela= tk.Tk()
-
 janela.title("Sistema de Alunos")
-
-janela.geometry("700x500")
+janela.geometry("1000x700")
 
 #-----------------------------
 #TÍTULO
-#----------------------------
+#---------------------------
 
 titulo = tk.Label(
     janela,
-    text="cadastro de Alunos",
+    text="Cadastro de Alunos",
     font=("Arial", 20)
 )
-
 titulo.pack(pady=20)
+
+#-------------------------
+# FRAME DAS ENTRADAS
+#------------------------
+
+frame_inputs = tk.Frame(janela)
+frame_inputs.pack(pady=10)
 
 #-------------------------
 #NOME
 #------------------------
 
 label_nome = tk.Label(
-    janela,
-    text="Nome"
+    frame_inputs,
+    text="Nome:"
+)
+label_nome.grid(
+    row=0,
+    column=0,
+    padx=5, 
+    pady=5, 
+    sticky="e"
 )
 
-label_nome.pack()
-
 campo_nome = tk.Entry(
-    janela,
+    frame_inputs,
     width=40
 )
 
-campo_nome.pack()
+campo_nome.grid(
+    row=0, 
+    column=1, 
+    padx=5, 
+    pady=5
+)
 
 #-----------------------
 #EMAIL
 #----------------------
 
 label_email = tk.Label(
-    janela,
-    text="Email"
+    frame_inputs,
+    text="Email:"
 )
 
-label_email.pack()
+label_email.grid(
+    row=1,
+    column=0,
+    padx=5,
+    pady=5,
+    sticky="e"
+)
 
 campo_email = tk.Entry(
-    janela,
+    frame_inputs,
     width=40
 )
 
-campo_email.pack()
+campo_email.grid(
+    row=1,
+    column=1,
+    padx=5,
+    pady=5
+)
 
 #---------------------
 #CURSO
 #--------------------
 
 label_curso = tk.Label(
-    janela,
-    text="curso"
+    frame_inputs,
+    text="Curso:"
 )
 
-label_curso.pack()
+label_curso.grid(
+    row=2,
+    column=0,
+    padx=5,
+    pady=5,
+    sticky="e"
+)
 
 campo_curso = tk.Entry(
-    janela,
+    frame_inputs,
     width=40
 )
 
-campo_curso.pack()
+campo_curso.grid(
+    row=2,
+    column=1,
+    padx=5,
+    pady=5
+)
+
+label_id = tk.Label(
+    frame_inputs,
+    text="ID:"
+)
+
+label_id.grid(
+    row=3,
+    column=0,
+    padx=5,
+    pady=5,
+    sticky="e"
+)
+
+campo_id = tk.Entry(
+    frame_inputs,
+    width=40
+)
+
+campo_id.grid(
+    row=3,
+    column=1,
+    padx=5,
+    pady=5
+)
 
 #----------------------
 #LISTA
@@ -101,7 +162,8 @@ def cadastrar():
     email= campo_email.get()
     curso = campo_curso.get()
     inserir_aluno(nome, email, curso)
-    print("Aluno cadastrado!")
+    lista.delete(0, tk.END)
+    lista.insert(tk.END, "Aluno cadastrado com sucesso!")
 
  #--------------------------
  # FUNÇÃO LISTAR
@@ -111,42 +173,123 @@ def cadastrar():
 def listar():
 
     lista.delete(0, tk.END)
-
     alunos = listar_alunos()
-
+    
     for aluno in alunos:
-
         texto = f"ID: {aluno[0]} | Nome: {aluno[1]} | Email: {aluno[2]} | Curso: {aluno[3]}"
-
         lista.insert(tk.END, texto)
+
+def buscar():
+    id_aluno = campo_id.get()
+    aluno = buscar_aluno_por_id(id_aluno)
+
+    if aluno:
+        texto = f"ID: {aluno[0]} | Nome: {aluno[1]} | Email: {aluno[2]} | Curso: {aluno[3]}"
+        lista.delete(0, tk.END)
+        lista.insert(tk.END, texto)
+    else:
+        lista.delete(0, tk.END)
+        lista.insert(tk.END, "Aluno não encontrado.")
+
+ #--------------------------
+ # FUNÇÃO ATUALIZAR
+ #--------------------------
+
+def atualizar():
+    id_aluno = campo_id.get()
+    aluno = buscar_aluno_por_id(id_aluno)
+
+    if aluno:
+        nome = campo_nome.get()
+        email = campo_email.get()
+        curso = campo_curso.get()
+
+        atualizar_aluno(id_aluno, nome, email, curso)
+        lista.delete(0, tk.END)
+        lista.insert(tk.END, "Aluno atualizado com sucesso!")
+    else:
+        lista.delete(0, tk.END)
+        lista.insert(tk.END, "Aluno não encontrado.")
+
+#--------------------------
+# FUNÇÃO EXCLUIR
+#--------------------------
+
+def deletar():
+    id_aluno = campo_id.get()
+    deletar_aluno(id_aluno)
+    lista.delete(0, tk.END)
+    lista.insert(tk.END, "Aluno deletado com sucesso!")
+
+#---------------------------
+# FRAME DOS BOTÕES
+#--------------------------
+
+frame_botoes = tk.Frame(janela)
+frame_botoes.pack(pady=20, anchor=tk.CENTER)
 
 #---------------------------
 #BOTÃO CADASTRAR
 #--------------------------
 
 botao_cadastrar = tk.Button(
-    janela,
+    frame_botoes,
     text="Cadastrar",
     command=cadastrar
 )
 
-botao_cadastrar.pack(pady=10)
+botao_cadastrar.pack(side=tk.LEFT, padx=10)
 
 #----------------------------
 #BOTÃO LISTAR
 #---------------------------
 
 botao_listar = tk.Button(
-    janela,
+    frame_botoes,
     text="Listar Alunos",
     command=listar
 )
 
-botao_listar.pack(pady=10)
+botao_listar.pack(side=tk.LEFT, padx=10)
+
+ #--------------------------
+ # BOTÃO BUSCAR
+ #--------------------------
+
+botao_buscar = tk.Button(
+    frame_botoes,
+    text="Buscar",
+    command=buscar
+)
+
+botao_buscar.pack(side=tk.LEFT, padx=10)
+
+
+#--------------------------
+# BOTÃO ATUALIZAR
+#--------------------------
+
+botao_atualizar = tk.Button(
+    frame_botoes,
+    text="Atualizar",
+    command=atualizar
+)
+botao_atualizar.pack(side=tk.LEFT, padx=10)
+
+ #--------------------------
+ # BOTÃO EXCLUIR
+ #--------------------------
+
+botao_excluir = tk.Button(
+    frame_botoes,
+    text="Excluir",
+    command=deletar
+)
+
+botao_excluir.pack(side=tk.LEFT, padx=10)
 
 #--------------------------
 #LOOP
 #--------------------------
 
 janela.mainloop()
- 
